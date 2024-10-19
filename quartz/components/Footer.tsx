ad603853ea8f127a98d03c7904e12d10ee7420ae
Jan "JoubaMety" Rašnovský <56211361+JoubaMety@users.noreply.github.com>
2024-10-19 12:46:43 +0200

    Quartz sync: Oct 19, 2024, 12:46 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -36,10 +36,10 @@ export default ((opts?: Options) => {
           <a href="https://brainmade.org/" target="_blank"><img id="brainmade" class="brainmade" src="/static/brainmade-light.svg"></img></a>
           <br></br>
           Commissioned illustration by <a href="https://coredeter.carrd.co/" target="_blank">coredeter</a> as a site icon/mascot.<br></br>
-          Using <a href="https://github.com/catppuccin/catppuccin" target="_blank">Catpuccin</a> color palette (<a href="https://github.com/catppuccin/catppuccin/blob/main/LICENSE" target="_blank">MIT License</a>).
+          Using <a href="https://github.com/catppuccin/catppuccin" target="_blank">Catpuccin</a> color palette (<a href="https://github.com/JoubaMety/notes.joubamety.com/blob/v4/licenses/catppuccin" target="_blank">MIT License</a>).
           <br></br>
           <br></br>
-          Website created using <a href="https://github.com/jackyzha0/quartz">Quartz v{version}</a> (<a href="https://github.com/JoubaMety/notes.joubamety.com/blob/v4/LICENSE.txt" target="_blank">MIT License</a>),<br></br>content made by <a href="https://joubamety.com" target="_blank">
+          Website created using <a href="https://github.com/jackyzha0/quartz">Quartz v{version}</a> (<a href="https://github.com/JoubaMety/notes.joubamety.com/blob/v4/licenses/quartz.txt" target="_blank">MIT License</a>),<br></br>content made by <a href="https://joubamety.com" target="_blank">
           &nbsp;
           <span>
             <img class="footer-avatar" src="/static/avatar-small.webp"></img>&nbsp;
